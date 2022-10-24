34bba9b41dcc34442926ad1d6f31c8de7a63f05c
RowDog <mrdiamonddirt@gmail.com>
2022-10-24 12:03:32 +0100

    removed export key word error

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
-  return (
-  <div>
-  <h1>I'm the root component</h1>
-  <Person name="Dan" age="34" />
-  <Person name="Stuart" age="30-something" />
-  <Person name="Ben" age="21" />
-  </div>
-  );
-  }
+  const App = () => {
+    return (
+    <div>
+    <h1>I'm the root component</h1>
+    <Person name="Dan" age="34" />
+    <Person name="Stuart" age="30-something" />
+    <Person name="Ben" age="21" />
+    </div>
+    );
+    }
+    
+    const Person = (props) => {
+    return (
+    <p> Hi I'm {props.name} and I'm {props.age} </p>
+    );
+    };
   
-  const Person = (props) => {
-  return (
-  <p> Hi I'm {props.name} and I'm {props.age} </p>
-  );
-  };
-  export
-
-export default App;
+  export default App;
