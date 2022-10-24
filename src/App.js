162982fdf783b7668b815c3c963130b173a785d4
RowDog <mrdiamonddirt@gmail.com>
2022-10-24 11:44:59 +0100

    class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React You Idiot
-        </a>
-      </header>
-    </div>
+class App extends React.Component { 
+
+  render() {
+return (
+  <div className="app"></div>
+  <h1>Hello</h1>
+  <person />
+  </div>
   );
 }
+}
 
+ const Person = () => {
+  return <p> I'm a functional component</p>
+ }
+ 
 export default App;
