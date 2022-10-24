442f7e5b3c5b2cdd9f22ed895d7f627635b739c3
RowDog <mrdiamonddirt@gmail.com>
2022-10-24 16:07:27 +0100

    added multiple examples

--- src/App.js
+++ src/App.js
@@ -1,32 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
-import './App.css' //import stylesheet
-import netflixFate from './images/netflixFate.png'
-import netflixGambit from './images/netflixGambit.png'
-import netflixOutside from './images/netflixOutside.png'
-import netflixStarTrek from './images/netflixStarTrek.png'
-import netflixWitcher from './images/netflixWitcher.png'
+import "./App.css";
+import "./App.css"; //import stylesheet
+import netflixFate from "./images/netflixFate.png";
+import netflixGambit from "./images/netflixGambit.png";
+import netflixOutside from "./images/netflixOutside.png";
+import netflixStarTrek from "./images/netflixStarTrek.png";
+import netflixWitcher from "./images/netflixWitcher.png";
+import bbcMorgan from "./images/bbcMorgan.png";
+import bbcPandemic from "./images/bbcPandemic.png";
+import bbcPay from "./images/bbcPay.png";
+import bbcUnilever from "./images/bbcUnilever.png";
+import airbnbHome from "./images/airbnbHome.png";
+import airbnbUnique from "./images/airbnbUnique.png";
+import airbnbCabin from "./images/airbnbCabin.png";
+import airbnbPets from "./images/airbnbPets.png";
+
+
 
 
 function App() {
   return (
-    <div className="images">
-    <Poster title={netflixFate}/>
-    <Poster title={netflixGambit}/>
-    <Poster title={netflixOutside}/>
-    <Poster title={netflixStarTrek}/>
-    <Poster title={netflixWitcher}/>
-    </div>
-  )
+    <>
+      <div className="images">
+        <Poster title={netflixFate} />
+        <Poster title={netflixGambit} />
+        <Poster title={netflixOutside} />
+        <Poster title={netflixStarTrek} />
+        <Poster title={netflixWitcher} />
+      </div>
+      <div className="bbcarticles">
+        <div className="bbcarticle">
+        <Bbc
+          image={bbcMorgan}
+          headline={"this is the headline"}
+          article={"this is the article"}
+        />
+        </div>
+        <div className="bbcarticle">
+        <Bbc
+          image={bbcPandemic}
+          headline={"this is the headline"}
+          article={"this is the article"}
+        />
+        </div>
+        <div className="bbcarticle">
+        <Bbc
+          image={bbcPay}
+          headline={"this is the headline"}
+          article={"this is the article"}
+        />
+        </div>
+        <div className="bbcarticle">
+        <Bbc
+          image={bbcUnilever}
+          headline={"this is the headline"}
+          article={"this is the article"}
+          />
+          </div>
+          </div>
+          <div className="airbnb">
+            <div className="airbnbitems">
+            <Airbnb image={airbnbHome} bnbtitle={<b>Entire Homes</b>}/>
+            </div>
+            <div className="airbnbitems">
+            <Airbnb image={airbnbUnique} bnbtitle={<b>Unique Stays</b>}/>
+            </div>
+            <div className="airbnbitems">
+            <Airbnb image={airbnbCabin} bnbtitle={<b>Cabins and cottages</b>}/>
+            </div>
+            <div className="airbnbitems">
+            <Airbnb image={airbnbPets} bnbtitle={<b>Pets allowed</b>}/>
+            </div>
+          </div>
+        </>
+  );
 }
 
 function Poster(props) {
-  console.log(props)
+  console.log(props);
   return (
     <>
-    <img src={props.title} alt=""/>
-  </>
-  )
+      <img src={props.title} alt="" />
+    </>
+  );
 }
 
-  export default App;
+function Bbc(props) {
+  console.log(props);
+  return (
+    <>
+      <img src={props.image} alt="" />
+      <h2>{props.headline}</h2>
+      <p>{props.article}</p>
+    </>
+  );
+}
+
+function Airbnb(props) {
+  return (
+    <>
+   <img src={props.image}/>
+   <p>{props.bnbtitle}</p> 
+    </>
+  )
+}
+export default App;
