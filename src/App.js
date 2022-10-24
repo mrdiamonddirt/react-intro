a0e5ad967478dc743c531b0ccd39752e31c7f500
RowDog <mrdiamonddirt@gmail.com>
2022-10-24 12:21:49 +0100

    passing from functional components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-  const App = () => {
-    return (
-    <div>
-    <h1>I'm the root component</h1>
-    <Person name="Dan" age="34" />
-    <Person name="Stuart" age="30-something" />
-    <Person name="Ben" age="21" />
-    </div>
-    );
-    }
-    
-    const Person = (props) => {
-    return (
-    <p> Hi I'm {props.name} and I'm {props.age} </p>
-    );
-    };
-  
+const App = () => {
+  return (
+  <div>
+  <h1>I'm the root component</h1>
+  <Person name="Dan" age="34" title="chef"/>
+  <Person name="Stuart" age="30-something" title="dictator" />
+  <Person name="Ben" age="21" title="cleaner" />
+  </div>
+  );
+  }
+
+  const Person = (props) => {
+  return (
+    <>
+  <p> Hi I'm {props.name} </p>
+  <Job title={props.title}/>
+    </>
+  );
+  };
+
+function Job(props){
+ return (
+  <p>i am a {props.title}</p>
+ )
+
+}
+
   export default App;
